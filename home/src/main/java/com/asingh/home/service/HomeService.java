676f21d6eab7bbf6f8526c1e6cfddd0be460ccc2
curiousAPIGuy <ashulko@gmail.com>
2025-07-29 12:42:48 -0700

    Delete home directory

--- home/src/main/java/com/asingh/home/service/HomeService.java
+++
@@ -1,7 +0,0 @@
-package com.asingh.home.service;
-
-public interface HomeService {
-	
-	public String getStatus();
-
-}
--- home/src/main/java/com/asingh/home/service/PasswordService.java
+++
@@ -1,7 +0,0 @@
-package com.asingh.home.service;
-
-public interface PasswordService {
-	
-	public String generatePass(String input);
-
-}
